8383be15090487154479464b77ad954b955fd23e
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-02-22 18:15:39 -0300

    The second commit.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api", function(req,res){
 
 
 app.get("/api/:date", function (req, res) {
-  req.params.date = new Date().toString()
+  req.params.date = new Date()
   res.json({unix: moment.unix(req.params.date), utc: moment.utc(req.params.date) });
   
 });
