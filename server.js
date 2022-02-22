a90489ac27288b3fc166b50366266d27f1e8c9b7
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-02-22 19:29:20 -0300

    .

--- server.js
+++ server.js
@@ -30,11 +30,9 @@ app.get("/api/:date", function (req, res) {
   let date = new Date(req.params.date)
   if (!String(date) || !Number.isInteger(date)){
     res.json({ error: "Invalid Date"})
-  } else if (date.toString()){
+  } else {
     res.json({unix: date.getTime(), utc: date.toUTCString() }); 
-  } else if (Number.isInteger(date)){
-    res.json()
-  }
+  } 
 });
 
 
