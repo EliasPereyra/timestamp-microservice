4385ed1a4a60894007b6676bcd08de1d4df818cd
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-02-22 19:09:35 -0300

    .

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api", function(req,res){
-
+  let date = new Date()
+  res.json({ unix: date.getTime(), utc: date.toUTCString()})
 })
 
 app.get("/api/:date", function (req, res) {
